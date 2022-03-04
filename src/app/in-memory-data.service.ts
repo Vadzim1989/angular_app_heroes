d4844966546b6597961c1cc905c62c5df5037ef1
Vadim Denisovski <vadim.denisovski@gmail.com>
2022-03-04 17:00:56 +0300

    change method genId

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -41,11 +41,11 @@ export class InMemoryDataService implements InMemoryDbService {
 
   // Overrides the genId method to ensure that a hero always has an id.
   // If the heroes array is empty,
-  // the method below returns the initial number (11).
+  // the method below returns the initial number (1).
   // if the heroes array is not empty, the method below returns the highest
   // hero id + 1.
   genId(heroes: Hero[]): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 1;
   }
 
   constructor() { }
